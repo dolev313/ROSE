c1489088fa2739071f866bb654e5d369ed909345
navehBrenner <navegerc@gmail.com>
2024-07-22 14:34:55 +0300

    added new driver modles - simulating moves forword and choosing max score move. on is bound to its own lane, the other can go between lanes

--- examples/none.py
+++ examples/none.py
@@ -1,15 +1,13 @@
 """
 This driver does not do any action.
 """
+
 from rose.common import obstacles, actions  # NOQA
 
 driver_name = "Driver"
 
-def printsome(word):
-    print(word)
+
 def drive(world):
-    # print("driving")
-    # printsome("driving")
     action = find_path(world)
     return action
 
@@ -17,7 +15,7 @@ def drive(world):
 def find_path(world):
     x_car = world.car.x
     y_car = world.car.y
-    if world.get((world.car.x, world.car.y)) == obstacles.PENGUIN:
+    if world.get((world.car.x, world.car.y - 1)) == obstacles.PENGUIN:
         return actions.PICKUP
 
     obs, pngu = extract_obs(world)
@@ -44,28 +42,19 @@ def find_path(world):
     return actions.NONE
 
 
-
-
-
-
-
 def extract_obs(world):
     obs = []
     pngu = []
     x, y = world.car.x, world.car.y
-    start_check = x - 1
-    end_check = x + 1
-    if x - 1 < 0:
-        start_check = x
-    if x + 1 > 6:
-        end_check = x
+    start_check = max(0, x - 1)
+    end_check = min(6, x + 1)
 
     for c in range(start_check, end_check):
-        try:
-            if world.get((c, y - 1)) == obstacles.PENGUIN:
-                pngu.append((c, y - 1))
-            elif world.get((c, y - 1)) != obstacles.NONE and world.get((c, y - 1)) != obstacles.PENGUIN:
-                obs.append((c, y - 1))
-        except IndexError:
-            pass
+        if world.get((c, y - 1)) == obstacles.PENGUIN:
+            pngu.append((c, y - 1))
+        elif (
+            world.get((c, y - 1)) != obstacles.NONE
+            and world.get((c, y - 1)) != obstacles.PENGUIN
+        ):
+            obs.append((c, y - 1))
     return obs, pngu
